ef97d3b3672d2321c8a87284981127565b067e16
decaffeinate <electrovesta@gmail.com>
2018-09-03 19:19:04 +0300

    decaffeinate: Convert index.coffee to JS

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-fs = require 'fs'
-path = require 'path'
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS205: Consider reworking code to avoid use of IIFEs
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const fs = require('fs');
+const path = require('path');
 
-module.exports = (robot, scripts) ->
-  scriptsPath = path.resolve(__dirname, 'src')
-  if fs.existsSync scriptsPath
-    for script in fs.readdirSync(scriptsPath).sort()
-      if scripts? and '*' not in scripts
-        robot.loadFile(scriptsPath, script) if script in scripts
-      else
-        robot.loadFile(scriptsPath, script)
+module.exports = function(robot, scripts) {
+  const scriptsPath = path.resolve(__dirname, 'src');
+  if (fs.existsSync(scriptsPath)) {
+    return (() => {
+      const result = [];
+      for (let script of Array.from(fs.readdirSync(scriptsPath).sort())) {
+        if ((scripts != null) && !Array.from(scripts).includes('*')) {
+          if (Array.from(scripts).includes(script)) { result.push(robot.loadFile(scriptsPath, script)); } else {
+            result.push(undefined);
+          }
+        } else {
+          result.push(robot.loadFile(scriptsPath, script));
+        }
+      }
+      return result;
+    })();
+  }
+};

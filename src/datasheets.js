a214d5739b99226a2414911c80ba63fbb237388b
kongr45gpen <electrovesta@gmail.com>
2018-09-06 13:54:15 +0300

    Add support for datasheets command to display multiple datasheets

--- src/datasheets.js
+++ src/datasheets.js
@@ -31,8 +31,9 @@ if (apikey == null) {
 }
 
 module.exports = robot =>
-    robot.respond(/datasheet (.*)/i, function(res) {
-        const part = res.match[1].toLowerCase().replace(/^\s+|\s+$/g, ""); // trim/strip
+    robot.respond(/datasheet(s?) (.*)/i, function (res) {
+        const multiple = res.match[1].toLowerCase().length !== 0;
+        const part = res.match[2].toLowerCase().replace(/^\s+|\s+$/g, ""); // trim/strip
 
         if ((robot.adapter !== undefined) && (robot.adapter.client !== undefined) && (robot.adapter.client.react !== undefined)) {
             // If the adapter supports it, add an hourglass reaction to the message, to show that the
@@ -44,8 +45,10 @@ module.exports = robot =>
             apikey,
             q: part,
             sortby: "score desc",
+            limit: 10,
             include: ['datasheets']
         };
+
 
         const respond = function(body) {
             // Octopart sends the data as JSON. Parse it.
@@ -57,73 +60,82 @@ module.exports = robot =>
                 return;
             }
 
-            // The first part that we received
-            const {
-                item
-            } = data.results[0];
+            let replies = []
 
-            // Make sure we return the most recent datasheet
-            // FIXME: This could be made much faster
-            const datasheets = item.datasheets.sort(function(a, b) {
-                const a_metadata = a.metadata || {};
-                const b_metadata = b.metadata || {};
-                // We don't need to parse the dates, as they are provided in a sortable string format
-                if (a_metadata.last_updated < b_metadata.last_updated) {
-                    return +1;
+            // For each part that we received
+            for (let result of data.results) {
+                let item = result.item
+                // Make sure we return the most recent datasheet
+                // FIXME: This could be made much faster
+                const datasheets = item.datasheets.sort(function (a, b) {
+                    const a_metadata = a.metadata || {};
+                    const b_metadata = b.metadata || {};
+                    // We don't need to parse the dates, as they are provided in a sortable string format
+                    if (a_metadata.last_updated < b_metadata.last_updated) {
+                        return +1;
+                    }
+                    if (a_metadata.last_updated > b_metadata.last_updated) {
+                        return -1;
+                    }
+                    return 0;
+                });
+
+                if (!datasheets.length) {
+                    replies.push(`No datasheets found for ${item.mpn}`);
+                    continue;
                 }
-                if (a_metadata.last_updated > b_metadata.last_updated) {
-                    return -1;
-                }
-                return 0;
-            });
 
-            if (!datasheets.length) {
-                res.reply(`No datasheets found for ${item.mpn}`);
-                return;
-            }
+                robot.logger.debug(`Most recent datasheet: ${(item.datasheets[0].metadata || {}).last_updated}`);
 
-            robot.logger.debug(`Most recent datasheet: ${(item.datasheets[0].metadata || {}).last_updated}`);
+                // Set up the price calculation
+                let price = undefined;
+                let price_currency = undefined;
 
-            // Set up the price calculation
-            let price = undefined;
-            let price_currency = undefined;
+                // Calculate for every currency, so that the preferred currency shows up first
+                for (let currency of Array.from(Object.keys(currencies))) {
+                    const arr = [];
 
-            // Calculate for every currency, so that the preferred currency shows up first
-            for (let currency of Array.from(Object.keys(currencies))) {
-                const arr = [];
+                    for (let offer of Array.from(item.offers)) {
+                        // Do not list prices that may be too high or low from unauthorized resellers
+                        if (!offer.is_authorized) {
+                            continue;
+                        }
 
-                for (let offer of Array.from(item.offers)) {
-                    // Do not list prices that may be too high or low from unauthorized resellers
-                    if (!offer.is_authorized) {
-                        continue;
+                        if (offer.prices[currency] !== undefined) {
+                            // Add the first (minimum order quantity) option
+                            arr.push(parseFloat(Object.values(offer.prices[currency])[0][1]));
+                        }
                     }
 
-                    if (offer.prices[currency] !== undefined) {
-                        // Add the first (minimum order quantity) option
-                        arr.push(parseFloat(Object.values(offer.prices[currency])[0][1]));
+                    if (arr.length !== 0) {
+                        // A price has been found!
+                        // Sort the prices so that the median can be easily calculated
+                        arr.sort();
+
+                        // Calculate median
+                        const half = parseInt(Math.floor(arr.length / 2.0));
+                        price = arr.length % 2 ? arr[half] : (arr[half - 1] + arr[half]) / 2.0;
+
+                        // Store the found currency so we can display the correct symbol later
+                        price_currency = currency;
+                        break;
                     }
                 }
 
-                if (arr.length !== 0) {
-                    // A price has been found!
-                    // Sort the prices so that the median can be easily calculated
-                    arr.sort();
+                // Show nothing if no price has been found
+                const price_string = (price === undefined) ? '' : ` (${price.toFixed(2)}${currencies[price_currency]})`;
 
-                    // Calculate median
-                    const half = parseInt(Math.floor(arr.length / 2.0));
-                    price = arr.length % 2 ? arr[half] : (arr[half - 1] + arr[half]) / 2.0;
+                // String formatted in Markdown
+                replies.push(`${item.datasheets[0].__class__} for **${item.mpn}**${price_string}: ${item.datasheets[0].url}`)
 
-                    // Store the found currency so we can display the correct symbol later
-                    price_currency = currency;
+                if (!multiple) {
+                    // Show one item only
                     break;
                 }
             }
 
-            // Show nothing if no price has been found
-            const price_string = (price === undefined) ? '' : ` (${price.toFixed(2)}${currencies[price_currency]})`;
-
-            // String formatted in Markdown
-            return res.reply(`${item.datasheets[0].__class__} for **${item.mpn}**${price_string}: ${item.datasheets[0].url}`);
+            // Send the combination of all the replies
+            return res.reply(replies.join("\n"));
         };
 
         return res.http("https://octopart.com/api/v3/parts/search")
--- test/datasheets-test.js
+++ test/datasheets-test.js
@@ -2,35 +2,62 @@ const Helper = require('hubot-test-helper');
 const chai = require('chai');
 const nock = require('nock')
 
-const { expect } = chai;
+const {
+  expect
+} = chai;
 
 const helper = new Helper('../src/datasheets.js');
 
 describe('datasheets', function() {
-  beforeEach(function (done) {
+  beforeEach(function() {
     nock.disableNetConnect();
-    nock('https://octopart.com/')
-      .get('/api/v3/parts/search?apikey=none&q=irlb8721&sortby=score%20desc&include=datasheets')
-      .replyWithFile(200, __dirname + '/replies/irlb8721.json', { 'Content-Type': 'application/json' });
-
-    // Timeout until the fake server repies
-    setTimeout(done, 1000)
+    let scope = nock('https://octopart.com/')
+      .get('/api/v3/parts/search?apikey=none&q=irlb8721&sortby=score%20desc&limit=10&include=datasheets')
+      .replyWithFile(200, __dirname + '/replies/irlb8721.json', {
+        'Content-Type': 'application/json'
+      })
+      .get('/api/v3/parts/search?apikey=none&q=stm32f103rb&sortby=score%20desc&limit=10&include=datasheets')
+      .replyWithFile(200, __dirname + '/replies/stm32f103rb.json', {
+        'Content-Type': 'application/json'
+      });
 
     return this.room = helper.createRoom();
   });
 
-  afterEach(function () {
+  afterEach(function() {
     nock.cleanAll();
     return this.room.destroy();
   });
 
-  return it('responds to datasheets', function () { //.then((() => done()), done)
+  it('responds to datasheet', function(done) {
     let room = this.room;
-    return this.room.user.say('alice', '@hubot datasheet IRLB8721').then(function () {
-      return expect(room.messages).to.eql([
-        ['alice','@hubot datasheet IRLB8721'],
-        ['hubot','@alice Datasheet for **IRLB8721PBF** (0.59€): http://datasheet.octopart.com/IRLB8721PBF-Infineon-datasheet-8326873.pdf']
-      ]);
+    this.room.user.say('alice', '@hubot datasheet IRLB8721').then(function() {
+      setTimeout(function() {
+        done();
+        return expect(room.messages).to.eql([
+          ['alice', '@hubot datasheet IRLB8721'],
+          ['hubot', '@alice Datasheet for **IRLB8721PBF** (0.61€): http://datasheet.octopart.com/IRLB8721PBF-Infineon-datasheet-8326873.pdf']
+        ]);
+      }, 50); // Timeout until the fake server replies
     });
+  });
+
+  it('responds to datasheets', function(done) {
+    let room = this.room;
+    this.room.user.say('alice', '@hubot datasheets STM32F103RB').then(function() {
+      setTimeout(function() {
+        done();
+        return expect(room.messages).to.eql([
+          ['alice', '@hubot datasheets STM32F103RB'],
+          ['hubot',
+            '@alice Datasheet for **STM32F103RBT7** (4.93€): http://datasheet.octopart.com/STM32F103RBT7-STMicroelectronics-datasheet-48026265.pdf\n' +
+            'Datasheet for **STM32F103RBT6** (5.64€): http://datasheet.octopart.com/STM32F103RBT6-STMicroelectronics-datasheet-48026265.pdf\n' +
+            'Datasheet for **STM32F103RBH6** (6.87€): http://datasheet.octopart.com/STM32F103RBH6-STMicroelectronics-datasheet-48026265.pdf\n' +
+            'Datasheet for **STM32F103RBH7** (4.49€): http://datasheet.octopart.com/STM32F103RBH7-STMicroelectronics-datasheet-48026265.pdf\n' +
+            'Datasheet for **STM32F103RBT7TR** (4.21€): http://datasheet.octopart.com/STM32F103RBT7TR-STMicroelectronics-datasheet-48026265.pdf'
+          ],
+        ]);
+      }, 50); // Timeout until the fake server replies
+    })
   });
 });
